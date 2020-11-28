e51a3a9f9448e43f6dd1469f43d4692fa56fe776
MGPlayzYT <51394649+MGPlayzYT@users.noreply.github.com>
2020-11-28 22:51:18 +0800

    Delete msg.js

--- commands/msg.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "msg",
-  devOnly: true,
-  cooldown: 0.0001,
-  execute: (message, args) => {
-    if (!args) return;
-    message.client.channels.cache.get("717911860443349033").send(args.join(" "));
-  }
-}
